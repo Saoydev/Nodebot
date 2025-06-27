282e2bb33af9c053616ebdf1b7503f144fc661e4
Space.blue <85410509+DeadlessNoob@users.noreply.github.com>
2025-06-27 19:57:22 -0400

    Gradients

--- src/events/on-ready/ready.js
+++ src/events/on-ready/ready.js
@@ -17,20 +17,20 @@ module.exports = {
             console.log('\x1b[38;2;255;255;255m%s\x1b[0m', `Failed to connect to mongoose: ${err}`);
         });
 
+        const frames = ['-', '\\', '|', '/'];
         let i = 0;
-        const frames = ['\\', '|', '/', '-'];
         const interval = setInterval(() => {
-            process.stdout.write(`\x1b[38;2;255;255;255mLoading\x1b[0m ${frames[i++ % frames.length]}`);
-        }, 100);
+            process.stdout.write(`\r\x1b[38;2;255;166;216mLoading\x1b[0m ${frames[i++ % frames.length]}`);
+        }, 200);
 
         setTimeout(() => {
             clearInterval(interval);
-            console.log('\n\x1b[1m\x1b[38;2;255;179;216m  _   _           _     ____        _     \x1b[0m');
-            console.log('\x1b[1m\x1b[38;2;255;179;216m  | \\ | | ___   __| | __| __ )  ___ | |_  \x1b[0m');
-            console.log('\x1b[1m\x1b[38;2;255;179;216m  |  \\| |/ _ \\ / _` |/ _ \\  _ \\ / _ \\| __| \x1b[0m');
-            console.log('\x1b[1m\x1b[38;2;255;179;216m  | |\\  | (_) | (_| |  __/ |_) | (_) | |_  \x1b[0m');
-            console.log('\x1b[1m\x1b[38;2;255;179;216m  |_| \\_|\\___/ \\__,_|\\___|____/ \\___/ \\__| \x1b[0m\n');
-            console.log('\x1b[1m\x1b[38;2;255;179;216m  Welcome to Nodebot  \x1b[0m\n');
+            console.log('\n\x1b[1m\x1b[38;2;255;102;153m  _   _           _     ____        _     \x1b[0m');
+            console.log('\x1b[1m\x1b[38;2;255;115;166m  | \\ | | ___   __| | __| __ )  ___ | |_  \x1b[0m');
+            console.log('\x1b[1m\x1b[38;2;255;128;179m  |  \\| |/ _ \\ / _` |/ _ \\  _ \\ / _ \\| __| \x1b[0m');
+            console.log('\x1b[1m\x1b[38;2;255;141;192m  | |\\  | (_) | (_| |  __/ |_) | (_) | |_  \x1b[0m');
+            console.log('\x1b[1m\x1b[38;2;255;153;204m  |_| \\_|\\___/ \\__,_|\\___|____/ \\___/ \\__| \x1b[0m\n');
+            console.log('\x1b[1m\x1b[38;2;255;166;216m  Hello, saoydev  \x1b[0m\n');
         }, 2000);
     },
 };
